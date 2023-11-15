e803b319ab4d40c58577aa83edc633e4752b65d1
Nuh <numoh.55@gmail.com>
2023-11-15 07:59:48 +0100

    Added My Profile
    My profile is implemented for the patient and the doctor can see all information about the patient.

--- vite-project/src/api/FetchPatients.jsx
+++ vite-project/src/api/FetchPatients.jsx
@@ -12,8 +12,29 @@ export async function getAllPatients() {
     return response.data;
   } catch (error) {
     if (error.response && error.response.status === 404) {
-      console.log("Could not find any doctor");
-      throw new Error("Could not find any doctor");
+      console.log("Could not find any patient");
+      throw new Error("Could not find any patient");
+    } else {
+      console.error(error.toJSON ? error.toJSON() : error);
+    }
+  }
+}
+
+export async function getOnePatient(id) {
+  console.log("This id: " + id);
+  try {
+    const options = {
+      method: "GET",
+      url: `http://localhost:8080/patient/get/${id}`,
+    };
+
+    const response = await axios.request(options);
+    console.log(response.data);
+    return response.data;
+  } catch (error) {
+    if (error.response && error.response.status === 404) {
+      console.log("Could not find any patient");
+      throw new Error("Could not find any patient");
     } else {
       console.error(error.toJSON ? error.toJSON() : error);
     }
---
+++ vite-project/src/pages/Profile/ProfileStyles.jsx
@@ -0,0 +1,24 @@
+import { styled } from "@mui/system";
+import { Box } from "@mui/material";
+
+export const ProfileContainer = styled("div")`
+  grid-area: main;
+  width: 100vw;
+  padding: 70px 60px 20px 60px; /* top right down left */
+  background-color: #f8f9fa;
+  color: #000000;
+  text-align: center;
+  height: 100%;
+`;
+
+export const ProfileWrapper = styled(Box)`
+  justify-content: center;
+  align-items: center;
+  background-color: #ffffff;
+  border: 1px solid #e0e0e0;
+  border-radius: 8px;
+  box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.1);
+  padding: 100px;
+  margin-left: 50px;
+  width: 100%
+`;
